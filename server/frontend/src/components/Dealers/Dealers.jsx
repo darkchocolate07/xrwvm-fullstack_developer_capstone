f3d7e83796a85cf8829dc11ddae99c06dceee34c
darkchocolate07 <abhirajgupta2004@gmail.com>
2024-11-01 14:12:48 -0400

    "Week 5"

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -32,7 +32,7 @@ const Dealers = () => {
     });
     const retobj = await res.json();
     if(retobj.status === 200) {
-      let all_dealers = Array.from(retobj.dealers)
+      let all_dealers = retobj.dealers
       let states = [];
       all_dealers.forEach((dealer)=>{
         states.push(dealer.state)
